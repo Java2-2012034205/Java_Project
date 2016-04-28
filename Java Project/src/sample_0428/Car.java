c7cffe57d8fef9073aaba399d437fc408365bf72
KIMMINSEOB <can0306@naver.com>
2016-04-28 21:06:25 +0900

    160428수업

---
+++ Java Project/src/sample_0428/Car.java
@@ -0,0 +1,18 @@
+package sample_0428;
+
+public class Car {
+    private String model;
+    private String color;
+    private int speed;
+    
+    private int id;
+    static int numbers = 0;
+    
+    public Car(String m, String c, int s){
+    	model = m;
+    	color =c;
+    	speed = s;
+    	id = ++numbers;
+    }
+
+}
