e02e2a6f2925540badb0410ed689eabb2a0c873a
KIMMINSEOB <can0306@naver.com>
2016-05-26 17:47:48 +0900

    계산기 과제

---
+++ Java Project/src/sample_0519/P_object.java
@@ -0,0 +1,16 @@
+package sample_0519;
+
+public class P_object {
+    int o_x, o_y;
+    
+    public P_object() {o_x=9; o_y=99;}
+    
+    public P_object(int o_x, o_y) {
+    	super();
+    	this.o_x=o_x;
+    	this.o_y=o_y;
+    }
+    
+    public void pr() {System.out.println("o_x : "+o_x+", o_y : "+o_y);}
+    public void change(int x, int y) {this.o_x; this.o_y=y;}
+}
